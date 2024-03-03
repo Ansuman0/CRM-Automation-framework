af1b07774d73df42859420632508c2030d7db2e3
Ansuman <ansuman07@hotmail.com>
2024-03-03 19:00:32 +0530

    Signed-off-by: Ansuman <ansuman07@hotmail.com>

---
+++ src/main/java/crm/objectRepository/ContactsPage.java
@@ -0,0 +1,107 @@
+package crm.objectRepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import crm.webdriverActions.WebDriverUtility;
+
+public class ContactsPage {
+
+	@SuppressWarnings("unused")
+	private WebDriverUtility webDriverUtility;
+
+	@FindBy(xpath = "//img[@title='Create Contact...']")
+	private WebElement createContactImg;
+
+	@FindBy(name = "firstname")
+	private WebElement firstNameEdt;
+
+	@FindBy(xpath = "//select[@name='salutationtype']")
+	private WebElement firstNameDrp;
+
+	@FindBy(name = "lastname")
+	private WebElement lastNameEdt;
+
+	@FindBy(xpath = "//input[@title='Save [Alt+S]']")
+	private WebElement saveBtn;
+
+	@FindBy(xpath = "//span[@class='dvHeaderText']")
+	private WebElement contactFormHeaderText;
+
+	@FindBy(xpath = "//input[@name='account_name']/following-sibling::Img[@title='Select']")
+	private WebElement orgLookUpImg;
+
+	@FindBy(name = "search_text")
+	private WebElement orgSearchEdt;
+
+	@FindBy(name = "search")
+	private WebElement orgSearchBtn;
+
+	public ContactsPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	// Business Library
+	public void clickOnCreateContactLookupImage() {
+		createContactImg.click();
+	}
+
+	public void enterFirstName(String firstName) {
+		firstNameEdt.sendKeys(firstName);
+	}
+
+	public void enterLastName(String lastName) {
+		lastNameEdt.sendKeys(lastName);
+	}
+
+	public void clickOnSaveButton() {
+		saveBtn.click();
+	}
+	public void selectSalutation(String salutation) {
+		WebDriverUtility.handleDropDown(firstNameDrp, salutation);
+	}
+
+	public void clickOnOrgLookupImage() {
+		orgLookUpImg.click();
+	}
+
+	public void createContact(WebDriver driver, String firstname, String lastname) {
+		WebDriverUtility.handleDropDown(firstNameDrp, "Mr.");
+		firstNameEdt.sendKeys(firstname);
+		lastNameEdt.sendKeys(lastname);
+		saveBtn.click();
+	}
+
+	public void createContactWithOrg(WebDriver driver, String firtname, String lastname, String orgname) {
+		WebDriverUtility.handleDropDown(firstNameDrp, "Mr.");
+		firstNameEdt.sendKeys(firtname);
+		lastNameEdt.sendKeys(lastname);
+		orgLookUpImg.click();
+		WebDriverUtility.switchToWindow(driver, "Accounts");
+		orgSearchEdt.sendKeys(orgname);
+		orgSearchBtn.click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+		driver.findElement(By.xpath("//a[.='" + orgname + "']")).click();
+
+		WebDriverUtility.switchToWindow(driver, "Contacts");
+		saveBtn.click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	public String getContactFormHeader() {
+		return contactFormHeaderText.getText();
+	}
+}
---
+++ src/test/java/crm/vtigerContactTests/CreateContactWithOrganization.java
@@ -0,0 +1,60 @@
+package crm.vtigerContactTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import crm.annotation.FrameworkAnnotation;
+import crm.enums.CategoryType;
+import crm.listeners.ListenerClass;
+import crm.objectRepository.ContactsPage;
+import crm.objectRepository.HomePage;
+import crm.objectRepository.OrganizationsPage;
+import crm.randomdataUtils.RandomUtilsImpliments;
+import crm.utilities.BaseClass;
+
+@Listeners(ListenerClass.class)
+public class CreateContactWithOrganization extends BaseClass {
+	
+	@FrameworkAnnotation(author = { "Ansuman" }, category = { CategoryType.SMOKE, CategoryType.REGRESSION })
+	@Test(groups = {"SMOKE","REGRESSION"})
+	public void createContactWithOrganizationTest() {
+
+		String orgname = RandomUtilsImpliments.getCompanyName();
+		String industryType = "Engineering";// can use data-provide for multipule data & run.
+
+		String firstName = RandomUtilsImpliments.getFirstName();
+		String lastname = RandomUtilsImpliments.getLastName();
+
+		/*
+		 * First we are creating organization based on that we will add contact.
+		 */
+		HomePage hp = new HomePage(driver);
+		hp.clickOnOrganizationsLink();
+
+		OrganizationsPage op = new OrganizationsPage(driver);
+
+		op.clickOnLeadLookUpImage();
+
+		op.createOrganizationWithIndustry(driver, orgname, industryType);
+
+		// Contact creatation based on the organization
+		hp.clickOnContactsLink();
+
+		ContactsPage cp = new ContactsPage(driver);
+		cp.clickOnCreateContactLookupImage();
+
+		cp.createContactWithOrg(driver, firstName, lastname, orgname);
+
+		System.out.println("Name : " + lastname);
+
+		// Validate
+		String contactHeader = cp.getContactFormHeader();
+		System.out.println(contactHeader);
+		Assert.assertTrue(contactHeader.contains(lastname));
+		System.out.println("Contact Header Matched : " + lastname);
+		// wUtil.takeScreenShot(driver, contactHeader);
+
+	}
+
+}
